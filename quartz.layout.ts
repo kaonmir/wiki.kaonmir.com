b2308882c9c90b77722287000d4d59e66110db6a
Jeff Son <sonjeff@naver.com>
2025-04-13 10:31:03 +0900

    댓글 컴포넌트를 추가하여 페이지 하단에 Giscus 기반의 댓글 기능을 통합하고, 프로필 스타일에서 불필요한 hover 효과를 제거하며, 폰트 크기를 조정했습니다. 또한, 상단 여백 변수를 6rem으로 변경했습니다.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: "giscus",
+      options: {
+        // from data-repo
+        repo: "kaonmir/wiki.kaonmir.com",
+        // from data-repo-id
+        repoId: "R_kgDOOXoGJA",
+        // from data-category
+        category: "Announcements",
+        // from data-category-id
+        categoryId: "DIC_kwDOOXoGJM4CpCif",
+      },
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/kaonmir/wiki.kaonmir.com",
