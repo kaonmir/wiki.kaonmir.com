4bb6601d34f6aca3d86d21a6e8b453cc5f0571b3
Jeff Son <sonjeff@naver.com>
2025-04-13 14:04:46 +0900

    프로필 스타일에서 폰트 크기를 상대 단위로 변경하고, 기본 스타일에서 폰트 크기를 추가했습니다. SSL 인증서 문서에서 인증 기관 표기를 이스케이프 처리하였으며, 레이아웃에서 불필요한 컴포넌트를 제거했습니다.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -45,9 +45,6 @@ export const defaultContentPageLayout: PageLayout = {
       condition: (page) => page.fileData.slug !== "index",
     }),
     Component.ArticleTitle(),
-    Component.ContentMeta({
-      showReadingTime: false,
-    }),
     Component.TagList(),
   ],
   left: [
